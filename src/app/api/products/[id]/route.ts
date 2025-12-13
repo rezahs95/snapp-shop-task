3044ccfef0dcc06f21f203b6b7a78a3c72fab851
Reza <rezahs1995@gmail.com>
2025-12-13 17:44:44 +0330

    fixed build errors

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { mockProducts } from "@/data/mock-products";
 
 export async function GET(
-  _request: Request,
-  { params }: { params: { id: string } }
+  _: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
   const product = mockProducts.find((product) => product.id === parseInt(id));
